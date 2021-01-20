a95dc66f884b01930fff241c448eeb8e6a0f84a2
yugojin <yugojinu5@gmail.com>
2021-01-20 11:39:00 +0900

    12章途中

---
+++ src/practice/NoCapsuleMan.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class NoCapsuleMan {
+	public String name;
+	public int age;
+
+	public NoCapsuleMan(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
