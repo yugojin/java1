b1b9d01ca3e156a5fe91d83b101a31173838284e
yugojin <yugojinu5@gmail.com>
2021-01-21 11:42:40 +0900

    fix 14章サンプル

--- src/polymorphism/human/Student.java
+++ src/polymorphism/human/Student.java
@@ -7,7 +7,7 @@ public class Student extends Human {
         super(name, age);
         this.score = score;
     }
-
+    @Override
     public String getProfile() {
         String profile = "年齢は" + super.age + "です。";
         profile += "学生で、テストの点数は" + this.score + "点です。";
