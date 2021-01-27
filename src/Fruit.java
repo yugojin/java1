77fabf365038c52e30054bbfd240fc27c50ef07b
yugojin <yugojinu5@gmail.com>
2021-01-27 20:59:39 +0900

    test

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,4 +1,16 @@
+import java.math.BigDecimal;
 
-public class Fruit {
+abstract class Fruit {
+	String name;
+	  BigDecimal price;
+
+	  Fruit(String name, BigDecimal price) {
+	    this.name = name;
+	    this.price = price;
+	  }
+
+	  abstract protected String getName();
+
+	  abstract protected BigDecimal getPrice();
 
 }
