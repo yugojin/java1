6544f0507008e15f2d94562fd6d184ff4c17bac0
yugojin <yugojinu5@gmail.com>
2021-01-31 10:33:20 +0900

    test

---
+++ src/User.java
@@ -0,0 +1,25 @@
+
+public class User {
+	private String name;
+    private String email;
+
+    public User () {
+    }
+
+    public String getName (){
+        return this.name;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public String getEmail (){
+        return this.email;
+    }
+
+    public void setEmail (String email){
+        this.email = email;
+    }
+
+}
